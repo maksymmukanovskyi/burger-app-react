3c600acd07636dd6c61d79e5dceb0bdd175e0036
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2021-03-28 13:24:04 -0600

    setup git hub pages

--- src/App.js
+++ src/App.js
@@ -4,18 +4,51 @@ import Person from './Person/Person'
 
 class App extends Component {
   state = {
+    on: false,
     persons: [
       {name: 'Max', age: 31},
       {name: 'Rob', age: 25},
       {name: 'Jenifer', age: 18},
     ]
   }
+
+  // switchHandler = (newName) => {
+  //   this.setState({
+  //     on: false,
+  //     persons: [
+  //       {name: newName, age: 31},
+  //       {name: 'Rob', age: 25},
+  //       {name: 'Jenifer', age: 18},
+  //     ]
+  //   })
+  // }
+  // nameHandler = ({target}) => {
+  //   this.setState({
+  //     persons: [
+  //       {name: target.value, age: 31},
+  //       {name: 'Rob', age: 25},
+  //       {name: 'Jenifer', age: 18},
+  //     ]
+  //   })
+  // }
+  togglePersons = () => {
+    this.setState({
+      on: !this.state.on,
+    })
+  }
   render() {
     
     return(
       <div className= 'App'>
-      <button>Click to show the name</button>
-      {this.state.persons.map(el => <Person name={el.name} age={el.age}/>)}
+
+      <button onClick={this.togglePersons}>Click to show the name</button>
+      {this.state.on &&
+       <Person>
+       {this.state.persons.map(el => 
+       <p key={el.name}>My name is {el.name} and I'm {el.age} years old!)</p>
+       )}
+       </Person>}
+      
       </div>
     )
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,8 @@
 import React from "react";
-const Person = ({name, age, children}) => {
+const Person = ({children}) => {
   return (
     <div>
-      <h1>I'm a {name} and i'm {age} y.o!</h1>
       <p>{children}</p>
-
     </div>
   );
 };
